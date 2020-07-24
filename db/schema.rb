5fcde7242d080b81153f7e072f424631307efc84
Mark Miranda <notmarkmiranda@gmail.com>
2020-07-23 18:43:37 -0600

    Add team model & seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_211112) do
+ActiveRecord::Schema.define(version: 2020_07_24_001823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2020_07_22_211112) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["league_id"], name: "index_seasons_on_league_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "city"
+    t.string "name"
+    t.string "abbreviation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/teams.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :team do
+    city { "MyString" }
+    name { "MyString" }
+    abbreviation { "MyString" }
+  end
+end
