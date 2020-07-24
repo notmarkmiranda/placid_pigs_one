9e81674cb970682b93a8602d062191c80c598a6d
Mark Miranda <notmarkmiranda@gmail.com>
2020-07-23 18:48:48 -0600

    Add #full_name to Team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Team, type: :model do
+  describe "methods" do
+    describe "#full_name" do
+      let(:team) { Team.new(abbreviation: "LAD", city: "Los Angeles", name: "Dodgers") }
+
+      it "shows a team's full name" do
+        expect(team.full_name).to eq("Los Angeles Dodgers")
+      end
+    end
+  end
 end
