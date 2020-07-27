0348cd181b5096d5f1fdb7c8a6886f408e2c19d2
Mark Miranda <notmarkmiranda@gmail.com>
2020-07-27 08:24:44 -0600

    Add users picks to season#show

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -4,7 +4,8 @@ class SeasonsController < ApplicationController
   def show
     @season = Season.find(params[:id]).decorate
     authorize @season
-    @picks = current_user.picks.where(game_week_id: @season.game_weeks.map(&:id))
+    #@picks = current_user.picks.where(game_week_id: @season.game_weeks.map(&:id))
+    @all_picks = Pick.where(game_week_id: @season.game_weeks.map(&:id))
   end 
 
   def new
--- app/models/pick.rb
+++ app/models/pick.rb
@@ -8,7 +8,11 @@ class Pick < ApplicationRecord
   validates_uniqueness_of :team_id, scope: [:user_id, :game_week_id]
   validates_presence_of :date
 
-  def self.by_date(date)
-    find_by(date: date)
+  def self.by_date(date, user_id=nil)
+    if user_id
+      find_by(date: date, user_id: user_id)
+    else
+      find_by(date: date)
+    end
   end
 end
