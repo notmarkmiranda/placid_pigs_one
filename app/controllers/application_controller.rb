98b922e41bafe1ec014c7bcda78d63738054b9f9
Mark Miranda <notmarkmiranda@gmail.com>
2020-08-07 10:06:01 -0600

    finish styling for admin winners

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   def require_admin!
+    return redirect_to dashboard_path unless current_user
     admin = current_user.memberships.where(role: 2).any?
     return redirect_to dashboard_path unless admin
   end
