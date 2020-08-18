cd1d22d8f720740e56b908f0a2c491a71da86eff
Mark Miranda <notmarkmiranda@gmail.com>
2020-08-17 20:00:24 -0600

    add specs for userseasonsetting model

--- spec/factories/user_season_settings.rb
+++ spec/factories/user_season_settings.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user_season_setting do
-    user { nil }
-    season { nil }
+    user 
+    season 
   end
 end
--- spec/models/user_season_setting_spec.rb
+++ spec/models/user_season_setting_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe UserSeasonSetting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe UserSeasonSetting, type: :model do
+  describe 'validations' do
+    before { create(:user_season_setting) }
+
+    it { should validate_uniqueness_of(:user_id).scoped_to(:season_id) }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :user }
+    it { should belong_to :season }
+  end
 end
