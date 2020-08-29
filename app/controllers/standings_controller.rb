9383573f103a016ad634075bccfe14a8e737dce3
Mark Miranda <notmarkmiranda@gmail.com>
2020-08-29 08:34:12 -0600

    Optimize standings call for standings_controller

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -2,6 +2,6 @@ class StandingsController < ApplicationController
   def show
     @season = Season.find(params[:id])
     authorize(@season)
-    @standings = @season.standings
+    @standings = @season.full_stats_standings
   end
 end
