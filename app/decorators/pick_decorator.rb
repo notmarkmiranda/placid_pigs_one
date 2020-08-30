34f59f34295db8d5e785002cfe4c03672e52f567
Mark Miranda <notmarkmiranda@gmail.com>
2020-08-29 21:17:24 -0600

    Add time zone to last updated

--- app/decorators/pick_decorator.rb
+++ app/decorators/pick_decorator.rb
@@ -4,7 +4,6 @@ class PickDecorator < ApplicationDecorator
   delegate_all
 
   def last_updated_date_and_time
-    return "never" unless pick
-    updated_time(pick.updated_at)
+    updated_time(pick.updated_at.in_time_zone("America/Denver"))
   end
 end
