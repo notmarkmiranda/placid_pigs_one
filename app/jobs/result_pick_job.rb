2707dddaec9555b195a36fe14dc034f772cf711f
Mark Miranda <notmarkmiranda@gmail.com>
2020-08-30 20:27:19 -0600

    Add ability to deactivate seasons

--- app/jobs/result_pick_job.rb
+++ app/jobs/result_pick_job.rb
@@ -2,9 +2,9 @@ class ResultPickJob < ApplicationJob
   queue_as :default
 
   def perform(result)
-    winner_picks = Pick.joins(:team).where("teams.abbreviation = ? AND picks.date = ?", result[:winner], result[:date]) 
-    loser_picks = Pick.joins(:team).where("teams.abbreviation = ? AND picks.date = ?", result[:loser], result[:date])
-    
+    winner_picks = Pick.joins(:team, game_week: :season).where("teams.abbreviation = ? AND picks.date = ? AND seasons.active = ?", result[:winner], result[:date], true) 
+    loser_picks = Pick.joins(:team, game_week: :season).where("teams.abbreviation = ? AND picks.date = ? AND seasons.active = ?", result[:loser], result[:date], true)
+
     winner_picks.update_all(status: :winner, game_token: result[:id])
     loser_picks.update_all(status: :loser, game_token: result[:id])
   end
